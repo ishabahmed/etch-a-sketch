771d03d17f5ceb5bb5d445d10061f4f59775e41f
Ishab Ahmed <43892687+ishabahmed@users.noreply.github.com>
2023-05-28 23:01:35 +0100

    Add ability to change between 1x1 to 100x100

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-const grid = document.querySelector('#grid');
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement('div');
-  div.classList.add('grid-item');
-  div.addEventListener('mouseover', () => {
-    div.setAttribute('style', 'background-color: #161616');
-  });
-  grid.appendChild(div);
-}
+const main = () => {
+  const clearBtn = document.querySelector('#clear');
+  clearBtn.addEventListener('click', clearGrid);
 
+  const sizeInput = document.querySelector('#size');
+  sizeInput.addEventListener('change', () => {
+    const sliderValue = document.querySelector('#slider-value');
+    sliderValue.textContent = sizeInput.value + ' x ' + sizeInput.value;
+    createGrid(sizeInput.value);
+  });
+
+  createGrid(16);
+};
+
+const createGrid = (size) => {
+  const grid = document.querySelector('#grid');
+  grid.innerHTML = '';
+  grid.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    div.addEventListener('mouseover', () => {
+      div.setAttribute('style', 'background-color: #161616');
+    });
+    grid.appendChild(div);
+  }
+};
+
+const clearGrid = () => {
+  const grid = document.querySelector('#grid');
+  grid.innerHTML = '';
+  createGrid(document.querySelector('#size').value);
+};
+
+document.addEventListener('DOMContentLoaded', main);
